a661158536a2040d5093968d12b9706ef3ebf955
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-04-22 13:21:23 +0200

    Update meddler.py

--- meddler.py
+++ meddler.py
@@ -1,5 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 #Class made to reduce the quality of given images, to create more images for a dataset
+
+#Storing the Images in the Subfolder as a List
+images = {}
+for x in os.listdir("Images"):
+    images[x] = cv2.imread("Images/" + x)
