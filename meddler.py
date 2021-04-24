f722c4c7e90b0259d1815559ba0844e0df7690a7
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-04-24 22:07:34 +0200

    Preprocessing Works
    Meddling and Creation of Dataset to do

--- meddler.py
+++ meddler.py
@@ -5,41 +5,52 @@ import os
 import random
 from tqdm.notebook import tqdm
 
-class Meddler:
-        
-    #Class made to reduce the quality of given images, to create more images for a dataset
+#Class made to reduce the quality of given images, to create more images for a dataset
 
-    #Storing the Images in the Subfolder as a List
-    images = {}
-    for image in tqdm(os.listdir("Images"), desc = 'Loading Data'):
-        images[image] = cv2.imread("Images/" + x)
+#Storing the Images in the Subfolder as a List
+images = {}
+for image in tqdm(os.listdir("Images"), desc = 'Loading Data'):
+    
+    images[image] = cv2.imread("Images/" + image, 0)    
+        
 
-        #Saving as Greyscale to facilitate thresholding and star classification
-        images[image] = cv2.cvtColor(images[image], cv2.COLOR_BGR2GRAY)
+img = images["0.png"].copy()
+Preprocess(img)
+
+#Thresholding the Image - Stars would remain to be white pixels 
+def Otsu(images):
+           
+    blur = cv2.GaussianBlur(images,(5,5),0).astype('uint8')
+    ret1,th1 = cv2.threshold(blur, 0, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
         
-class Preprocessor:
+    cv2.imwrite("Output/Preprocessed/Otsutest.png" , th1, [cv2.IMWRITE_PNG_COMPRESSION, 0])
         
-    #Threshodling the Image - Stars would remain to be white pixels 
-    def Otsu(images):
-        
-        counter = 0
-        
-        blur = cv2.GaussianBlur(images,(5,5),0)
-        ret1,th1 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        counter += 1
-        
-        cv2.imwrite("Images/Preprocessed/otsu" + counter + ".png", th1, [cv2.IMWRITE_PNG_COMPRESSION, 0])
-        
-        imgs = [blur, 0, th1]
-        titles = ['Gaussian filtered Image','Histogram','Otsu Thresholding']
+    imgs = [blur, 0, th1]
+    titles = ['Gaussian filtered Image','Histogram','Otsu Thresholding']
          
-        for i in xrange(1):
+    for i in range(1):
             
-            plt.subplot(3,3,i*3+1),plt.imshow(imgs[i*3],'gray')
-            plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
-            plt.subplot(3,3,i*3+2),plt.hist(imgs[i*3].ravel(),256)
-            plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
-            plt.subplot(3,3,i*3+3),plt.imshow(imgs[i*3+2],'gray')
-            plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+1),plt.imshow(imgs[i*3],'gray')
+        plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+2),plt.hist(imgs[i*3].ravel(),256)
+        plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+        plt.subplot(3,3,i*3+3),plt.imshow(imgs[i*3+2],'gray')
+        plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
             
-        plt.show()
+    plt.show()
+        
+def EdgeRecognition(images):
+  
+    edges = cv2.Canny(images,100,200)
+    
+    cv2.imwrite("Output/Preprocessed/Edges/testedges.png", edges, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
+    
+def Preprocess(images):
+    
+    Otsu(images)
+    EdgeRecognition(images)
+
