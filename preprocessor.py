a4e0e722c88b1db895af7a82b73919df7612f3df
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2021-06-07 21:02:56 +0200

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,122 @@
+#%%
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+preprocessed = {}
+for processed in preprocessed:
+    
+    preprocessed[processed] = cv2.imread("Output/" + processed, 0)
+
+#%%
+#PREPROCESSING THE IMAGE
+image = cv2.imread("Images/1.png")
+counter = 0
+
+#  constants
+BINARY_THRESHOLD = 20
+CONNECTIVITY = 4
+DRAW_CIRCLE_RADIUS = 4
+
+#  convert to gray
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#  extract edges
+binary_image = cv2.Laplacian(gray_image, cv2.CV_8UC1)
+
+#  fill in the holes between edges with dilation
+dilated_image = cv2.dilate(binary_image, np.ones((5, 5)))
+
+#  threshold the black/ non-black areas
+_, thresh = cv2.threshold(dilated_image, BINARY_THRESHOLD, 255, cv2.THRESH_BINARY)
+
+#  find connected components
+components = cv2.connectedComponentsWithStats(thresh, CONNECTIVITY, cv2.CV_32S)
+
+#  draw circles around center of components
+#see connectedComponentsWithStats function for attributes of components variable
+centers = components[3]
+for center in centers:
+    cv2.circle(thresh, (int(center[0]), int(center[1])), DRAW_CIRCLE_RADIUS, (255), thickness=-1)
+    
+counter += 1
+cv2.imwrite("Images/res1.png", thresh)
+cv2.imshow("result", thresh)
+cv2.waitKey(0)
+
+#%%
+#DETECTING BRIGHT PIXELS
+from imutils import contours
+from skimage import measure
+import imutils
+
+image = cv2.imread("Images/res1.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+
+
+thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
+
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=4)
+
+# perform a connected component analysis on the thresholded
+# image, then initialize a mask to store only the "large" components
+labels = measure.label(thresh, background=0)
+mask = np.zeros(thresh.shape, dtype="uint8")
+# loop over the unique components
+for label in np.unique(labels):
+	# if this is the background label, ignore it
+	if label == 0:
+		continue
+	# otherwise, construct the label mask and count the
+	# number of pixels 
+	labelMask = np.zeros(thresh.shape, dtype="uint8")
+	labelMask[labels == label] = 255
+	numPixels = cv2.countNonZero(labelMask)
+	# if the number of pixels in the component is sufficiently
+	# large, then add it to our mask of "large blobs"
+	if numPixels > 300:
+		mask = cv2.add(mask, labelMask)
+
+# find the contours in the mask, then sort them from left to right
+cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+for method in ("left-to-right", "right-to-left", "top-to-bottom", "bottom-to-top"):
+    (cnts, boundingBoxes) = contours.sort_contours(cnts, method = method)
+# loop over the contours
+for (i, c) in enumerate(cnts):
+	# draw the bright spot on the image
+	(x, y, w, h) = cv2.boundingRect(c)
+	((cX, cY), radius) = cv2.minEnclosingCircle(c)
+	cv2.circle(image, (int(cX), int(cY)), int(radius),
+		(0, 0, 255), 3)
+	cv2.putText(image, "#{}".format(i + 1), (x, y - 15),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+# show the output image
+cv2.imwrite("Preprocessed/2.png", image, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# %%
+def PointPlotter(img):
+
+    image = cv2.imread(img)
+    points = [[], []]
+
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+
+            if np.all(image[x][y] == (0, 0, 255)):
+                shaper = (x, y)
+                points.append(shaper)
+
+    plt.plot(points[0],points[1])
+    plt.show()
+
+
+PointPlotter("Preprocessed/2.png")
+
+# %%
